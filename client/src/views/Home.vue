8b1a861fbdd461fcc3774ed07f37d645de568497
Jared <hubermaj2@hawkmail.newpaltz.edu>
2020-11-30 16:27:19 -0500

    11-30

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -4,10 +4,10 @@
 
     <h1 class="title">
       </h1>
-      <p v-if="session.user" class="subtitle">
+      <p  class="subtitle">
         Click "Record" above to begin tracking your exercise!
       </p>
-      <p v-else class="subtitle">
+      <p  class="subtitle">
         Sign up or log in to begin!
       </p>
       
