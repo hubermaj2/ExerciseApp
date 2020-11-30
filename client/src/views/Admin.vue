12604b2e13719f0e580ab731b20533809e7073f8
Jared <hubermaj2@hawkmail.newpaltz.edu>
2020-11-29 22:22:25 -0500

    11-29

--- client/src/views/Admin.vue
+++ client/src/views/Admin.vue
@@ -1,30 +1,48 @@
 
-<script src="..\..\..\server\models\admin.js" type="text/javascript"></script>
+<script src="..\..\..\server\models\admin.js"></script>
 <template>
-  <div class="admin">
+  <div id="submit" class="admin">
     <h1>Admin Tools</h1>
 
     <h1 class="title">
-        This is the Admin Tools module. When the server side is implemented, Admins will be able to add, edit, and delete users, as well as view their exercise activity.
+        Edit exercises
       </h1>
       <div><br>Add an Exercise:</div>
-      <form action = ''  onsubmit="return submit(document.getElementById('textbox').value);">
-        <input id=textbox class="input" type="text" placeholder="Text input">
-        <input class="input" type="submit" value="Submit">
-      </form>
+        <input v-model="exercisename" type="text" ref="my_input">
+        <button v-on:click="submit">Submit</button>
   </div>
 </template>
 
 <script>
-//import admin from "..\..\..\server\models\admin.js"
+
+import admin from "../../../server/models/admin.js"
+import axios from "axios"
+import submitbackend from "../../../server/models/admin.js"
 // @ is an alias to /src
 // document.getElementById('textbox').value
 export default {
   name: 'Admin',
+  data: {
+    exercisename: ''
+  },
   components: {
-    
+
   },
   methods:{
+      submit: function (event){
+        submitbackend(this.exercisename)
+      /*  axios.put('http://192.168.0.6:6969/theme', 
+    {
+      theme: this.state.theme
+    }
+    )
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.log(error);
+    });*/
+      }
   }
 }
 </script>
--- server/models/admin.js
+++ server/models/admin.js
@@ -2,7 +2,8 @@ const mysql = require('./mysql');
 
 const PREFIX = process.env.MYSQL_TABLE_PREFIX || 'Fall2020_';
 
-async function submit(data){
+
+export async function submitbackend(data){
     const sql = `INSERT INTO ${PREFIX}Exercise_Types (created_at, Name, Type, Muscle_Group, Video_Url, Relative_Difficulty) VALUES (CURRENT_TIMESTAMP, ${data}, Cardio, Legs, videourl, 1);`
     return await mysql.query(sql, [data]);
 }
