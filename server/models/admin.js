748f55945f0ff1a112e885cc8645e2d8ffb7ab7a
Jared <hubermaj2@hawkmail.newpaltz.edu>
2020-11-30 10:34:50 -0500

    11-30

--- server/models/admin.js
+++ server/models/admin.js
@@ -5,7 +5,7 @@ const PREFIX = process.env.MYSQL_TABLE_PREFIX || 'Fall2020_';
 
 async function submitbackend(data){
     const sql = `INSERT INTO ${PREFIX}Exercise_Types (created_at, Name, Type, Muscle_Group, Video_Url, Relative_Difficulty) VALUES (CURRENT_TIMESTAMP, ${data}, Cardio, Legs, videourl, 1);`
-    alert("hello");
+    console.log("hello");
     return await mysql.query(sql, [data]);
 }
 
